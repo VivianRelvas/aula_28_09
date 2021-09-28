08ecf5c1a34ff81b7457abb25e30dabbcbf24690
VivianRelvas <vivianrelvas@hotmail.com>
2021-09-28 17:57:02 +0000

    Criando o arquivo Lista

--- src/main/java/application/respositories/LivroRepository.java
+++ src/main/java/application/respositories/LivroRepository.java
@@ -1,5 +1,5 @@
 package application.respositories;
-import org.springframework.data.repository.CrudRepository;   //Salva, deleta e atualiza
+import org.springframework.data.repository.CrudRepository;   //CRUD - Criar, Ler, Atualizar e Deletar 
 
 import application.models.Livro;
 public interface LivroRepository extends CrudRepository<Livro, Integer> {
